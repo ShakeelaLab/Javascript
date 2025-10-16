c80a09eacf0644ab84027c8675dd543f06a116be
ShakeelaLab <shakeela@live.nl>
2025-10-16 11:45:02 +0200

    gemaakt

--- functions.js
+++ functions.js
@@ -25,14 +25,31 @@ console.log(school);
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
 
+function getCompliment(name){
+  return (`Lekker bezig met die functies, ${name}!`);
+}
 
+const compliment = getCompliment("Shakeela")
+
+console.log(compliment);
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
+function getGrades() {
+  return [8, 8.5, 6, 7];
+}
 
+const grades = getGrades();
+console.log(grades);
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+function getDetails() {
+  const names = { firstName: 'Jan', lastName: 'Janssen' };
+  return (`${names.firstName} ${names.lastName}`);
+}
+
+console.log(getDetails());
